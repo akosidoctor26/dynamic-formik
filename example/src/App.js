13d9fa98a48b874ad220fee5347acd7b4dc1d5f3
Kristine Doc <mskristinedoctor@gmail.com>
2020-08-20 10:46:32 -0700

    working example

--- example/src/App.js
+++ example/src/App.js
@@ -10,18 +10,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+        <DynamicFormik>{(formi) => <h1>Finally!</h1>}</DynamicFormik>
+      </div>
     </div>
   );
 }
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import babel from '@rollup/plugin-babel';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import packageJSON from './package.json';
 
 export default {
@@ -19,12 +18,11 @@ export default {
     }
   ],
   plugins: [
-    peerDepsExternal(),
     nodeResolve({ extensions: ['.jsx', '.js'] }),
-    commonjs(),
     babel({
       exclude: 'node_modules/**',
       presets: ['@babel/env', '@babel/preset-react']
-    })
+    }),
+    commonjs()
   ]
 };
