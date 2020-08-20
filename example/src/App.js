c635328eefa0171e07dd9f7ce4a62a3beded9699
Kristine Doc <mskristinedoctor@gmail.com>
2020-08-20 10:56:04 -0700

    working setup

--- example/src/App.js
+++ example/src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 
 import './App.css';
 
 import DynamicFormik from 'dynamic-formik';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <div>
-        <DynamicFormik>{(formi) => <h1>Finally!</h1>}</DynamicFormik>
-      </div>
-    </div>
-  );
+  return <DynamicFormik>{(formi) => <h1>Finally!</h1>}</DynamicFormik>;
 }
 
 export default App;
